a33d60bf04d85d778341b5aeaad8dbb72b3afb40
fawadmehmood1 <fawad.mehmood@stixor.com>
2024-10-02 10:37:29 +0500

    initial setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,17 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Roboto_Condensed, Cormorant } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const robotoCondensed = Roboto_Condensed({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+});
+
+const cormorant = Cormorant({
+  subsets: ["latin"],
+  variable: "--font-cormorant",
+  weight: ["300", "400", "500", "600", "700"],
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +25,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${robotoCondensed.className} ${cormorant.variable}`}>
+        {children}
+      </body>
     </html>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,9 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
+  corePlugins: {
+    container: false,
+  },
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -13,8 +16,60 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+
+      colors: {
+        primary: "#1A1A1A",
+        secondary: "#E8CFB5",
+        accent: "#74604B",
+        white: "#ffffff",
+      },
+
+      fontFamily: {
+        cormorant: ["var(--font-cormorant)"],
+      },
+
+      fontSize: {
+        hero: ["4rem", "4.8rem"],
+        title: ["3rem", "3.6rem"],
+        subtitle: ["2.25rem", "0px"],
+        "body-lg": ["1.375rem", "25.78px"],
+        body: ["1rem", "0rem"],
+      },
     },
   },
-  plugins: [],
+  plugins: [
+    function ({ addComponents }: any) {
+      addComponents({
+        ".container": {
+          width: "100%",
+          paddingLeft: "1rem",
+          paddingRight: "1rem",
+          marginLeft: "auto",
+          marginRight: "auto",
+          "@screen sm": {
+            maxWidth: "100%",
+            paddingLeft: "2rem",
+            paddingRight: "2rem",
+          },
+          "@screen md": {
+            maxWidth: "100%",
+          },
+          "@screen lg": {
+            maxWidth: "100%",
+            paddingLeft: "2.625rem",
+            paddingRight: "2.625rem",
+          },
+          "@screen xl": {
+            maxWidth: "100%",
+            paddingLeft: "5.9375rem",
+            paddingRight: "5.9375rem",
+          },
+          "@screen 2xl": {
+            maxWidth: "1536px",
+          },
+        },
+      });
+    },
+  ],
 };
 export default config;
