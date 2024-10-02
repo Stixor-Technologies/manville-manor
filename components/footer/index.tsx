a5ea18ef672030f280671a7c4fcbe3b43adb1781
fawadmehmood1 <fawad.mehmood@stixor.com>
2024-10-02 17:29:19 +0500

    minor ui update in footer

--- components/footer/index.tsx
+++ components/footer/index.tsx
@@ -31,7 +31,7 @@ const Footer = () => {
         </div>
 
         <div>
-          <p className="leadiing-[2.0625rem] mt-9 text-center text-[1.375rem] font-medium text-white/65">
+          <p className="leadiing-[2.0625rem] mt-1 text-center text-[1.375rem] font-medium text-white/65 lg:mt-9">
             Term & Condition - Privacy Policy &copy;{year} All rights reserved
             by <span className="text-white">Manville Manor &copy; {year}</span>
           </p>
