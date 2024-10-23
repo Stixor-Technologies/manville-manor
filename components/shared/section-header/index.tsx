82a2a337e58aa3b8cf0fd1d0cce0ceb711c367ad
fawadmehmood1 <fawad.mehmood@stixor.com>
2024-10-23 17:12:19 +0500

    added animation for secton headers

--- components/shared/section-header/index.tsx
+++ components/shared/section-header/index.tsx
@@ -1,4 +1,9 @@
+"use client";
 import React, { FC } from "react";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+gsap.registerPlugin(ScrollTrigger);
 
 interface SectionHeaderProps {
   header: string;
@@ -6,10 +11,30 @@ interface SectionHeaderProps {
 }
 
 const SectionHeader: FC<SectionHeaderProps> = ({ header, description }) => {
+  useGSAP(() => {
+    const tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: "[data-animated-text]",
+        start: "top 90%",
+      },
+    });
+    tl.from("[data-animated-text]", {
+      opacity: 0,
+      y: 50,
+      ease: "power1",
+      duration: 0.8,
+    });
+  }, []);
+
   return (
     <>
-      <h2 className="text-center text-xl text-secondary">{header}</h2>
-      <p className="mt-4 text-center font-cormorant text-[2rem] font-bold leading-tight text-white sm:text-5xl">
+      <h2 data-animated-text className="text-center text-xl text-secondary">
+        {header}
+      </h2>
+      <p
+        data-animated-text
+        className="mt-4 text-center font-cormorant text-[2rem] font-bold leading-tight text-white sm:text-5xl"
+      >
         {description}
       </p>
     </>
