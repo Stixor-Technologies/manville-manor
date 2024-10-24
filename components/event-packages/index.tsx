3249b11dbf58886879377776b56dd63c8e0af92a
fawadmehmood1 <fawad.mehmood@stixor.com>
2024-10-24 14:23:17 +0500

    added animations for events packages

--- components/event-packages/index.tsx
+++ components/event-packages/index.tsx
@@ -1,28 +1,72 @@
-import React, { FC } from "react";
+"use client";
+import React, { FC, useRef } from "react";
 import { PackageCard } from "../package-card";
 import SectionHeader from "../shared/section-header";
 import { venuePackage, decorPackage } from "@/utils/utils";
 import { cn } from "@/lib/utils";
+
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+gsap.registerPlugin(ScrollTrigger);
 
 interface EventPackages {
   fromHome?: boolean;
 }
 
 const EventPackages: FC<EventPackages> = ({ fromHome }) => {
+  const packagesContainer = useRef<HTMLDivElement | null>(null);
+
+  useGSAP(
+    () => {
+      const packageSection = gsap.timeline({
+        scrollTrigger: {
+          trigger: packagesContainer?.current,
+          start: "top 85%",
+          markers: true,
+        },
+      });
+
+      packageSection.from("[data-animated-package-text]", {
+        opacity: 0,
+        y: 50,
+        ease: "power1",
+        duration: 0.8,
+      });
+
+      packageSection.from(
+        "[data-animated-package-card]",
+        {
+          opacity: 0,
+          y: 50,
+          duration: 0.8,
+          ease: "power1",
+          stagger: 0.3,
+        },
+        0.3,
+      );
+    },
+    { scope: packagesContainer },
+  );
+
   return (
-    <>
+    <div ref={packagesContainer}>
       {fromHome ? (
         <SectionHeader header="Pricing" description="Event Packages" />
       ) : (
-        <h2 className="text-center text-3xl text-secondary md:text-5xl">
+        <h2
+          data-animated-package-text
+          className="text-center text-3xl text-secondary md:text-5xl"
+        >
           Best Plans For Events
         </h2>
       )}
 
       <div
+        data-animated-package-card
         className={cn(
           "flex flex-col items-center justify-center gap-9 md:flex-row",
-          fromHome ? "mt-[87px]" : "mt-[4.25rem]",
+          fromHome ? "mt-[5.4375rem]" : "mt-[4.25rem]",
         )}
       >
         <PackageCard variant={"decor"} eventPackage={venuePackage}>
@@ -44,7 +88,7 @@ const EventPackages: FC<EventPackages> = ({ fromHome }) => {
           </h2>
         </PackageCard>
       </div>
-    </>
+    </div>
   );
 };
 
