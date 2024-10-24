90c0104f7c157cdd8e03382308f1de6b0684a02a
fawadmehmood1 <fawad.mehmood@stixor.com>
2024-10-24 20:04:17 +0500

    added metrices

--- app/gallery/page.tsx
+++ app/gallery/page.tsx
@@ -2,12 +2,16 @@ import React from "react";
 import Hero from "@/components/shared/hero";
 import AboutHero from "@/public/assets/about/about-hero.png";
 import GallerySlider from "@/components/gallery/gallery-slider/page";
+import EssentialMetrics from "@/components/gallery/essential-metrics/essential-metrics";
 
 const Gallery = () => {
   return (
     <>
       <Hero heroImage={AboutHero} heroHeading="Gallery" />
-      <GallerySlider />
+      <div className="container px-8 lg:px-16 xl:px-[8.125rem]">
+        <EssentialMetrics />
+      </div>
+      {/* <GallerySlider /> */}
     </>
   );
 };
---
+++ components/gallery/essential-metrics/essential-metrics.tsx
@@ -0,0 +1,59 @@
+"use client";
+import React, { useRef } from "react";
+import { gsap, ScrollTrigger } from "gsap/all";
+import { useGSAP } from "@gsap/react";
+
+const EssentialMetrics = () => {
+  gsap.registerPlugin(ScrollTrigger);
+  const metricsContainer = useRef<HTMLDivElement | null>(null);
+
+  useGSAP(
+    () => {
+      const slides: HTMLDivElement[] = gsap.utils.toArray(".metric");
+      ScrollTrigger.create({
+        trigger: metricsContainer.current,
+        start: "top 90%",
+        animation: gsap.from(slides, {
+          textContent: 0,
+          duration: 3,
+          ease: "circ.inOut",
+          snap: { textContent: 1 },
+        }),
+      });
+    },
+    { dependencies: [metricsContainer] },
+  );
+
+  return (
+    <div
+      ref={metricsContainer}
+      className="ml- mx-auto my-10 flex w-full max-w-[46.1875rem] justify-between sm:mt-20 lg:mb-11 lg:mt-[179px]"
+    >
+      <div className="metric-container !w-fit !pl-0">
+        <h5 className="metric-number text-nowrap">
+          +<span className="metric">10</span> Years
+        </h5>
+
+        <span className="metric-label">Experience</span>
+      </div>
+
+      <div className="metric-container">
+        <h5 className="metric-number">
+          +<span className="metric">450</span>
+        </h5>
+
+        <span className="metric-label">Customers</span>
+      </div>
+
+      <div className="metric-container">
+        <h5 className="metric-number">
+          +<span className="metric">15</span>K
+        </h5>
+
+        <span className="metric-label">Portfolio Photos</span>
+      </div>
+    </div>
+  );
+};
+
+export default EssentialMetrics;
