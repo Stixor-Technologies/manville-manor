f8c31c1081353def9f256c4cad6161573c90cd6d
fawadmehmood1 <fawad.mehmood@stixor.com>
2024-11-01 12:29:35 +0500

    added about section on home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,29 @@
-// import Contact from "@/components/shared/contact";
-// import Faqs from "@/components/shared/faq";
-// import OfficeLocation from "@/components/home-page/office-location";
-// import SportsArticle from "@/components/home-page/sports-article";
-// import EventPackages from "@/components/event-packages";
-// import HomeGallery from "@/components/home-page/home-gallery";
-// import VenueDetails from "@/components/home-page/venue-details";
-// import BackDrops from "@/components/home-page/backdrops";
-// import HomeHero from "@/components/home-page/home-hero";
+import Contact from "@/components/shared/contact";
+import Faqs from "@/components/shared/faq";
+import OfficeLocation from "@/components/home-page/office-location";
+import SportsArticle from "@/components/home-page/sports-article";
+import EventPackages from "@/components/event-packages";
+import HomeGallery from "@/components/home-page/home-gallery";
+import VenueDetails from "@/components/home-page/venue-details";
+import BackDrops from "@/components/home-page/backdrops";
+import HomeHero from "@/components/home-page/home-hero";
+import HomeAbout from "@/components/home-page/home-about/home-about";
 
 export default function Home() {
   return (
     <main className="min-h-screen flex-col items-center justify-between overflow-hidden">
-      {/* <HomeHero />
+      <HomeHero />
+      <HomeAbout />
       <HomeGallery />
-      <VenueDetails />
       <BackDrops />
+      <VenueDetails />
       <div className="container px-8 lg:px-16 xl:px-[8.125rem]">
         <EventPackages fromHome />
         <SportsArticle />
         <Contact />
         <Faqs />
         <OfficeLocation />
-      </div> */}
+      </div>
     </main>
   );
 }
