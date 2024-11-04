38cb7a60a77df1c948a434c4a41734960d884a17
fawadmehmood1 <fawad.mehmood@stixor.com>
2024-11-04 16:10:36 +0500

    added home page

--- components/home-page/office-location/index.tsx
+++ components/home-page/office-location/index.tsx
@@ -8,24 +8,24 @@ import Link from "next/link";
 const OfficeLocation = () => {
   return (
     <section>
-      <div className="grid grid-cols-[0.95fr_1fr_0.08fr] grid-rows-[1fr_0.5fr_0.5fr] text-white">
-        <Image
-          src={Location}
-          alt="location-bg"
-          className="col-span-3 col-start-1 row-span-2 row-start-1 rounded-sm"
-        />
+      <div className="grid grid-cols-1 grid-rows-[1fr_0.4fr_0.4fr_auto] text-white sm:grid-cols-[0.95fr_1fr_0.08fr] sm:!grid-rows-[1fr_0.5fr_0.5fr] xs:grid-rows-[1fr_0.15fr_0.15fr_auto]">
+        <picture className="col-span-3 col-start-1 row-span-2 row-start-1 rounded-sm">
+          <source media="(min-width: 640px)" srcSet="/assets/location.png" />
+          <source srcSet="/assets/location-mobile.png" />
+          <img />
+        </picture>
 
-        <div className="col-start-2 row-span-2 row-start-2 flex max-w-xl flex-col justify-center bg-accent/50 px-4 pr-4  backdrop-blur-lg lg:pl-12">
-          <h4 className="text-[clamp(12px,1.4vw,20px)] font-semibold ">
+        <div className="col-start-2 row-span-2 row-start-2 mx-auto flex w-[90%] max-w-xl flex-col justify-center bg-accent/50 px-4 pr-4 text-center backdrop-blur-lg sm:mx-0 sm:w-full  sm:text-left lg:pl-12">
+          <h4 className="text-[clamp(22px,1.4vw,22px)] font-semibold ">
             Location
           </h4>
-          <p className="font-cormorant text-[clamp(12px,2.5vw,36px)] leading-tight">
+          <p className="mt-2 font-cormorant text-[clamp(22px,2.5vw,36px)] leading-tight md:mt-4">
             Located in the heart of Manville, NJ, Manville Manor is where
             elegance meets excellence.
           </p>
         </div>
 
-        <div className="col-start-1 row-start-3 mt-9 flex max-w-fit flex-col items-start gap-2 self-start text-white/65 lg:text-[1.375rem] xl:flex-row xl:gap-4">
+        <div className="col-start-2 row-start-4 mx-auto mt-9 flex w-[90%] max-w-fit flex-col items-center gap-4 text-center text-[1.375rem] text-white/65 sm:col-start-1 sm:row-start-3 sm:mx-0 sm:w-full sm:items-start sm:gap-4 sm:self-start sm:text-base lg:text-[1.375rem] xl:flex-row xl:gap-4">
           <div className="flex items-center gap-2">
             <Image src={SMS} alt="sms-icon" />
             <Link href="mailto:conference2022@design.com">
