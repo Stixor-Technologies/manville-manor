5843df2639861de46f48f6917d9ab985ef704224
fawadmehmood1 <fawad.mehmood@stixor.com>
2024-12-09 15:42:43 +0500

    fixed build errors

--- app/payment/page.tsx
+++ app/payment/page.tsx
@@ -1,85 +1,11 @@
-"use client";
-import React, { useEffect, useState } from "react";
-import { loadStripe } from "@stripe/stripe-js";
-import { Elements } from "@stripe/react-stripe-js";
-import CheckoutForm from "@/components/payment/checkout-form";
-import { Appearance, StripeElementsOptions } from "@stripe/stripe-js";
-import { STRIPE_PUBLISHABLE_KEY } from "@/utils/contants";
-import { useSearchParams } from "next/navigation";
-import { getInvoice } from "@/utils/api-calls";
-import Spinner from "@/components/shared/spinner";
-
-// Make sure to call loadStripe outside of a component’s render to avoid
-// recreating the Stripe object on every render.
-// This is your test publishable API key.
-const stripePromise = loadStripe(STRIPE_PUBLISHABLE_KEY!);
+import { Suspense } from "react";
+import PaymentPage from "@/components/payment/payment-page";
 
 const Payment = () => {
-  const [clientSecret, setClientSecret] = useState("");
-  const [isLoading, setisLoading] = useState(true);
-  const [invoiceAmount, setInvoiceAmount] = useState(0);
-  const [isPaid, setIsPaid] = useState(false);
-
-  const searchParams = useSearchParams();
-  const bookingId = searchParams.get("bookingId");
-
-  useEffect(() => {
-    (async () => {
-      if (bookingId) {
-        try {
-          setisLoading(true);
-          const resp = await getInvoice(Number(bookingId));
-          if (resp) {
-            setInvoiceAmount(resp?.invoice?.totalPrice);
-            setIsPaid(resp?.isPaid);
-            const res = await fetch("/api/payment-intent", {
-              headers: { "Content-Type": "application/json" },
-              method: "POST",
-              body: JSON.stringify({ amount: resp?.invoice?.totalPrice }),
-            });
-            const data = await res.json();
-            setClientSecret(data?.clientSecret);
-          }
-        } catch (error) {
-          console.error("error", error);
-        } finally {
-          setisLoading(false);
-        }
-      }
-    })();
-  }, []);
-
-  const appearance: Appearance = {
-    theme: "stripe",
-    variables: {
-      colorPrimary: "#74604B",
-      colorBackground: "#282828",
-      colorText: "#ffffff",
-    },
-  };
-  const options: StripeElementsOptions = {
-    clientSecret,
-    appearance,
-  };
-
   return (
-    <div>
-      {isLoading ? (
-        <div className="flex min-h-[23.9375rem] items-center justify-center">
-          <Spinner />
-        </div>
-      ) : (
-        clientSecret && (
-          <Elements options={options} stripe={stripePromise}>
-            <CheckoutForm
-              amount={invoiceAmount}
-              bookingId={Number(bookingId)}
-              isPaid={isPaid}
-            />
-          </Elements>
-        )
-      )}
-    </div>
+    <Suspense>
+      <PaymentPage />
+    </Suspense>
   );
 };
 
--- app/payment/payment-success/page.tsx
+++ app/payment/payment-success/page.tsx
@@ -1,48 +1,11 @@
-"use client";
-import React, { useEffect } from "react";
-import { Button } from "@/components/button";
-import { useSearchParams } from "next/navigation";
-import { useRouter } from "next/navigation";
-import { updatePaymentStatus } from "@/utils/api-calls";
+import PaymentSuccessPage from "@/components/payment/payment-success-page";
+import { Suspense } from "react";
 
 const PaymentSuccess = () => {
-  const searchParams = useSearchParams();
-  const router = useRouter();
-  const amount = searchParams.get("amount");
-  const bookingId = searchParams.get("bookingId");
-
-  useEffect(() => {
-    const fetchInvoice = async () => {
-      if (bookingId) {
-        try {
-          await updatePaymentStatus(Number(bookingId));
-        } catch (error) {
-          console.error("error", error);
-        }
-      }
-    };
-
-    fetchInvoice();
-  });
-
   return (
-    <>
-      <div className="p-3 text-center text-white">
-        <h1 className="mb-2 text-4xl font-extrabold">Thank you!</h1>
-        <h2 className="text-2xl">Your payment was successful</h2>
-
-        <div className="my-2">
-          <span>Amount Paid:</span> {`$${amount}`}
-        </div>
-        <Button
-          onClick={() => router.replace("/")}
-          size={"lg"}
-          className="mt-6"
-        >
-          Move to Homepage
-        </Button>
-      </div>
-    </>
+    <Suspense>
+      <PaymentSuccessPage />
+    </Suspense>
   );
 };
 
