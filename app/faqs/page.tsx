10919d1b02f25f868860e023818a6b2beb912349
fawadmehmood1 <fawad.mehmood@stixor.com>
2024-12-18 11:28:23 +0500

    added faq page

---
+++ app/faqs/page.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Hero from "@/components/shared/hero";
+import HeroFaq from "@/public/assets/faq-hero.jpg";
+import Faqs from "@/components/shared/faq";
+
+const FaqsPage = () => {
+  return (
+    <div>
+      <Hero heroImage={HeroFaq} heroHeading="" />
+      <div className="container my-10 md:my-20">
+        <Faqs />
+      </div>
+    </div>
+  );
+};
+
+export default FaqsPage;
