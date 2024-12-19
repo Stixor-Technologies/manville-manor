037f1d61d33303c389e20a6bc22847815ced12e5
fawadmehmood1 <fawad.mehmood@stixor.com>
2024-12-19 12:22:54 +0500

    added header for venue page

--- components/venue/venue-detail/index.tsx
+++ components/venue/venue-detail/index.tsx
@@ -31,13 +31,24 @@ const VenueDetail = () => {
   }, [searchParams]);
 
   return (
-    <>
+    <div className="flex-1">
+      <div className="mb-10 flex flex-col items-center gap-4">
+        <h2 className="text-secondary">Venue</h2>
+
+        <h3 className=" font-cormorant text-5xl text-white">Venue Details</h3>
+
+        <p className="text-center text-[22px] text-secondary">
+          Manville Manor Amenities - Modern Functionality, amusement, and
+          entertainment
+        </p>
+      </div>
+
       {loading ? (
         <div className="flex-1 py-36">
           <Spinner />
         </div>
       ) : venueDetail ? (
-        <div className="mx-auto grid flex-1 grid-cols-1 place-items-center gap-9 md:grid-cols-[repeat(auto-fill,_minmax(20rem,_1fr))] md:place-items-stretch">
+        <div className="mx-auto grid grid-cols-1 place-items-center gap-9 md:grid-cols-[repeat(auto-fill,_minmax(20rem,_1fr))] md:place-items-stretch">
           <div className="w-full max-w-[374px] bg-accent px-5 py-8">
             <h2 className="mb-11 text-center text-[1.375rem] font-semibold text-white">
               Amenities
@@ -92,7 +103,7 @@ const VenueDetail = () => {
           Venue Detail Not Found
         </p>
       )}
-    </>
+    </div>
   );
 };
 
